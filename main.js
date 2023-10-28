33f07d34060a08c2149342df7663e72536ac3798
Rafael Gonzalez <rafandresgon@gmail.com>
2023-10-28 14:11:40 -0300

    segunda pre-entrega

--- main.js
+++ main.js
@@ -1,40 +1,111 @@
 
 
-let entrada = prompt("Ingrese el usuario");
+// let entrada = prompt("Ingrese el usuario");
 
-while(entrada != "Usuario"){
-    alert("Usuario Incorrecto");
+// while(entrada != "Usuario"){
+//     alert("Usuario Incorrecto");
 
-    entrada = prompt("Ingrese el usuario");
+//     entrada = prompt("Ingrese el usuario");
 
-}
+// }
 
-    alert("¡Bienvenido!");
+//     alert("¡Bienvenido!");
 
-let opcion = prompt(`
-Ingrese la opcion Requerida
-1. Ropa Mujer
-2. Ropa Hombre
-`);
+// let opcion = prompt(`
+// Ingrese la opcion Requerida
+// 1. Ropa Mujer
+// 2. Ropa Hombre
+// `);
 
-switch (opcion){
-    case "1":
-        alert("Tenemos ropa, calsado accesorios");
-        break;
+// switch (opcion){
+//     case "1":
+//         alert("Tenemos ropa, calsado accesorios");
+//         break;
 
-        case "2":
-            alert("Tenemos calsados y ropa")
-            break;
+//         case "2":
+//             alert("Tenemos calsados y ropa")
+//             break;
 
-            default:
-                alert("Opcion Incorrecta")
-                break;
-}
+//             default:
+//                 alert("Opcion Incorrecta")
+//                 break;
+// }
 
-for(let turno = 1; turno <= 3; turno++){
-    let nombre = prompt("Ingrese su Nombre");
+// for(let turno = 1; turno <= 3; turno++){
+//     let nombre = prompt("Ingrese su Nombre");
 
-    alert(`Pagar en caja #${turno} Sr: ${nombre}`);
-}
+//     alert(`Pagar en caja #${turno} Sr: ${nombre}`);
+// }
 
-alert("Turnos terminados, por favor intentar mañana");
+// alert("Turnos terminados, por favor intentar mañana");
+
+
+            //***** */ segunda pre-entrega
+
+            //!! funciones matematicas
+
+
+//     function operacionesMtematicas(){
+//         let numero1 = parseFloat(prompt("Ingrese un numero"));
+
+//         let operacion = prompt("Introduce el la operación (+, -, *, /):")
+
+//         let numero2 = parseFloat(prompt("Ingrese un segundo numero numero"));
+
+//         switch(operacion){
+//             case "+":
+//                 alert("Resultado: " + (numero1 + numero2));
+//                 break;
+
+//             case "-":
+//                 alert("Resultado: " + (nuumero1 - numero2));
+//                 break;
+//             case "*":
+//             alert("Resultado: " + (numero1 * numero2));
+//             break;
+
+//             case "/":
+//                 if(numero2 !==0){
+//                     alert("Resultado " + (numero1 / numero2));
+//                 } else {
+//                     alert("No es posible dividir por cero.");
+//                 }
+//                 break;
+//             default:
+//                 alert("Operacion no valida");
+
+//         }
+// }
+
+//     operacionesMtematicas()
+    // !!
+
+
+    //!! objetos
+
+//     let usuario = {
+//         nombre: "Rafael",
+//         apellido: "Gonzalez",
+//         edad: "21"
+//     };
+
+// let propiedad = prompt("Ingrese la propiedad que quiere buscar dentro del Array");
+
+// if (!usuario.hasOwnProperty(propiedad)) {
+//     alert("Propiedad no encontrada");
+// }else{
+// alert(usuario[propiedad]);
+// }
+// !!
+
+//!! arrays  
+
+// let numeros = [1, 2, 3, 4, 5 ,6];
+// let index = numeros.indexOf(1);
+// console.log(index);
+
+// let existe = numeros.includes(3);
+// console.log(existe);
+
+// let filtrado = numeros.filter(numero => numero > 1);
+// console.log(filtrado);
